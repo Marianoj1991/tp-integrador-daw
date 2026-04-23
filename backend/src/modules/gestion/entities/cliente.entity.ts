70ddca319abf37722f01f9c234155aa883f3a6d2
MacarenaSchefer <maca.schefer@hotmail.com>
2026-04-22 21:18:14 -0300

    Creacion de entidades y tarea.servicies desde rama_macarena a tester

---
+++ backend/src/modules/gestion/entities/cliente.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { EstadosClientesEnum } from "../enums/estados-clientes.enum";
+import { Proyecto } from "./proyecto.entity";
+
+
+
+
+@Entity({name: "clientes"})
+export class Cliente {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    nombre!: string;
+
+    @Column({type: 'enum', enum: EstadosClientesEnum})
+    estado!: EstadosClientesEnum
+
+    @OneToMany(() => Proyecto, (proyecto) => proyecto.cliente)
+    proyectos!: Proyecto []
+    
+
+}
