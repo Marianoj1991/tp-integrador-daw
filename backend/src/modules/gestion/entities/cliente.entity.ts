e4f57eb02dd3bea87f6579a2058e32edde52ab11
Mariano Jimenez <104471721+Marianoj1991@users.noreply.github.com>
2026-04-25 17:29:10 -0300

    Cliente.service.ts implementado

--- backend/src/modules/gestion/entities/cliente.entity.ts
+++ backend/src/modules/gestion/entities/cliente.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import { EstadosClientesEnum } from '../enums/estados-clientes.enum';
 import { Proyecto } from './proyecto.entity';
 
